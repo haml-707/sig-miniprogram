e71417343d50ea1c39977714d1bb90e3da8392ca
haml-707 <845831435@qq.com>
2022-01-05 11:01:53 +0800

    new year banner
    new year banner

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,22 +8,34 @@ Page(mixin({
         imgUrls: [
             {
                 type: 1,
-                url: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/%E5%B0%8F%E7%A8%8B%E5%BA%8F%E9%A6%96%E9%A1%B5%E6%96%B0banner.png'
+                url: 'https://openeuler-website-beijing.obs.cn-north-4.myhuaweicloud.com/%E6%AC%A7%E6%8B%89%E6%96%B0%E5%B9%B4%E8%A7%86%E9%A2%910.3.mp4',
+                poster: 'https://openeuler-website-beijing.obs.cn-north-4.myhuaweicloud.com/detail-banner/video-poster.png'
             },
             {
                 type: 2,
-                url: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/openEuler%E8%B4%A1%E7%8C%AE%E8%80%85%E6%96%B0%E6%98%A5%E7%A4%BC%E7%89%A90204b.mp4',
-                poster: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/video-poster/2.png'
+                url: 'https://openeuler-website-beijing.obs.cn-north-4.myhuaweicloud.com/detail-banner/mini-banner.png',
+                src:'../newyear/newyear'
             },
             {
-                type: 2,
-                url: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/IMG_7242.MP4',
-                poster: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/video-poster/1.png'
-            }
+                type: 3,
+                url: 'https://openeuler-website-beijing.obs.cn-north-4.myhuaweicloud.com/detail-banner/%E5%B0%8F%E7%A8%8B%E5%BA%8F%E5%B7%A5%E5%8D%A1%E5%A5%97banner.png',
+                src:'../card/card'
+            },
         ],
         iphoneX: false,
         meetingConponent: null,
         autoplay: true
+    },
+    handleContact (e) {
+        if (!sessionUtil.getUserInfoByKey('access')) {
+            wx.navigateTo({
+                url: '/pages/auth/auth'
+            })
+            return;
+        }
+        wx.navigateTo({
+            url: e.currentTarget.dataset.src
+        })
     },
     swithTab(e) {
         if (!sessionUtil.getUserInfoByKey('access')) {
@@ -37,6 +49,7 @@ Page(mixin({
         })
     },
     previewImage(e) {
+        
         const current = e.target.dataset.src //获取当前点击的 图片 url
         wx.previewImage({
             current,
